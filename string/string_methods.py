4c05bd2b7625688a2e195b757d8ef473bd787fd8
AkshayK025 <askanchan314@gmail.com>
2026-01-20 08:57:47 +0530

    sting_methods updated

--- string/string_methods.py
+++ string/string_methods.py
@@ -1,2 +1,24 @@
-a = 'Elon'
-print(f'{a} A genius')
+string_a = 'Hello Elon'
+
+# Basic String Methods 
+# len(),str()
+print("Basic String Methods")
+print(len(string_a),
+      str(string_a),
+      type(string_a),
+      sep='\n')
+
+# String Case Methods
+print()
+print("String Case Methods")
+string_a = "hello elon"
+
+print(
+    f"upper(): {string_a.upper()}",
+    f"lower(): {string_a.lower()}",
+    f"capitalize(): {string_a.capitalize()}",
+    f"title(): {string_a.title()}",
+    f"swapcase(): {string_a.swapcase()}",
+    sep=",\n"
+)
+
