7f29620ca4ce3505b42dbdd07e1142f297b6d44f
AkshayK025 <askanchan314@gmail.com>
2026-01-20 09:20:14 +0530

    search and find methods added to the file

--- string/string_methods.py
+++ string/string_methods.py
@@ -9,11 +9,12 @@ print(len(string_a),
       sep='\n')
 
 # String Case Methods
+# upper(),lower(),capitalize(),title(),swapcase()
 print()
 print("String Case Methods")
 string_a = "hello elon"
 
-print(
+print(string_a,
     f"upper(): {string_a.upper()}",
     f"lower(): {string_a.lower()}",
     f"capitalize(): {string_a.capitalize()}",
@@ -22,3 +23,16 @@ print(
     sep=",\n"
 )
 
+# Search and Find Methods
+# find():,rfind():,index():,rindex():,str.endswith(suffix),str.startswith(prefix)
+print()
+string_b = string_a + " " + 'hello'
+print(string_b)
+print(f'find(hello):{string_b.find('hello')}',
+      f'rfind(hello):{string_b.rfind('hello')}',
+      f'index(l):{string_b.index('l')}',
+      f'rindex(l):{string_b.rindex('l')}',
+      f'endswith(hello):{string_b.endswith('hello')}',
+      f'startswith(hello):{string_b.startswith('hello')}',
+      sep='\n'
+      )
